0efd1cbdaf3dae2a0f5833fafcad1cb8c6448eb8
fasgithub <francesco.fasulo96@gmail.com>
2019-05-20 12:48:51 +0200

    Initial work on Local Sandbox

---
+++ src/org/protor/sandbox/francesco/MyVehicleType.java
@@ -0,0 +1,7 @@
+package org.protor.sandbox.francesco;
+
+public enum MyVehicleType {
+	AERIAL,
+	TERRESTRIAL,
+	MARINE
+}
---
+++ src/org/protor/sandbox/francesco/tests/Test01.java
@@ -0,0 +1,24 @@
+package org.protor.sandbox.francesco.tests;
+
+import org.protor.sandbox.francesco.MyVehicle;
+import org.protor.sandbox.francesco.MyVehicleType;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		
+		MyVehicle v1 = new MyVehicle(MyVehicleType.TERRESTRIAL);
+		v1.setName("Fiat 700");
+		
+		MyVehicle v2 = new MyVehicle("Eurofighter", MyVehicleType.AERIAL);
+		
+		System.out.println("v1 - Name: " + v1.getName());
+		System.out.println("v1 - Type: " + v1.getType());
+		
+		System.out.println("v2 - Name: " + v2.getName());
+		System.out.println("v2 - Type: " + v2.getType());
+		
+
+	}
+
+}
